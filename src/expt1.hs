1004672b07b2fe1c9476e88ab49abedd7dec8d15
mr-road <you@example.com>
2013-11-01 11:07:57 +0000

    Got it working

--- src/expt1.hs
+++ src/expt1.hs
@@ -1,6 +1,9 @@
-transform :: [(a,b)] -> [(a,[b])]
-transform x =
-	let uniqueAs = nub $ fmap (fst) x
-		findBs inp list = map (snd . (filter (\(f,s)-> f == inp))) list
-	in  
-		map (\(l,r) -> (l, findBs l x)) (uniqueAs x)
+import Data.List
+
+transformUserIDProductIDToUserProducts :: Eq a => [(a,b)] -> [(a,[b])]
+transformUserIDProductIDToUserProducts tupleList =
+	let uniqueAs inp = (nub $ fmap (fst) inp)
+	    filter' inp = filter (\(f,s)-> f == inp)
+	    findBs inp list = map (snd) (filter' inp list)
+	in
+		map (\uid -> (uid, findBs uid tupleList)) (uniqueAs tupleList)
